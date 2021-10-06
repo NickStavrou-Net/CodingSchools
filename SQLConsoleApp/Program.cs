4835fc59c1afe1665cf1e2a98cd8c2bd7731ead9
nick stavrou <nickstavrou@gmail.com>
2021-10-06 20:44:08 +0300

    Add project files.

---
+++ SQLConsoleApp/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace SQLConsoleApp
+{
+    class Program
+    {
+        static readonly string connectionstring = @"Server= DESKTOP-6VLMGU5\SQLEXPRESS;Database= Database200;Trusted_Connection=True;";
+        static SqlConnection sqlConnection = new SqlConnection(connectionstring);
+
+        static void Main(string[] args)
+        {
+            using (sqlConnection)
+            {
+                try
+                {
+                    sqlConnection.Open();
+
+                    //Insert User
+                    Console.WriteLine("Enter Username and Password: ");
+                    string userName = Console.ReadLine();
+                    string password = Console.ReadLine();
+
+                    SqlCommand cmdInsert = new SqlCommand($"INSERT INTO UsersDetails(LoginName, PasswordHash) " +
+                        $"VALUES('{userName}', '{password}')", sqlConnection);
+
+                    int insertedRows = cmdInsert.ExecuteNonQuery();//<------edo ksekinaei na rotaei tin Database 
+                    if (insertedRows > 0)
+                    {
+                        Console.WriteLine("Insertion Succeful");
+                        Console.WriteLine($"{insertedRows} rows inserted succesfully");
+                    }
+
+                    //Select
+                    SqlCommand cmdSelect = new SqlCommand("SELECT COUNT (*) FROM UsersDetails " +
+                        "WHERE Username = @username AND Password = @password", sqlConnection);
+
+                    cmdSelect.Parameters.AddWithValue("@username", "nick");
+                    cmdSelect.Parameters.AddWithValue("@password", "123");
+
+                    int result = (int)cmdSelect.ExecuteScalar();//<----------to kano CAST ston integer eauto tou
+                    if (result > 0)
+                    {
+                        Console.WriteLine("Found User");
+                    }
+                    else
+                    {
+                        Console.WriteLine("User not Found");
+                    }
+
+                    //login
+                    Console.WriteLine("Enter Usernmame and Password to Login: ");
+                    string usernameInserted = Console.ReadLine();
+                    string passwordInserted = Console.ReadLine();
+
+                    SqlCommand cmdLogin = new SqlCommand($"SELECT ID, Username, Password FROM UsersDetails " +
+                        $"WHERE Username ='{usernameInserted}' AND Password = '{passwordInserted}'", sqlConnection);
+
+                    SqlDataReader reader = cmdLogin.ExecuteReader();//<----arxikopoiisi antikeimenou kai me me mia method pou epistrefei object
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("User Logged In Sucessfully");
+                        //Create a User Object
+                        User user = new User
+                        {
+                            ID = reader.GetInt32(0),
+                            Username = reader.GetString(1),
+                            Password = reader.GetString(2)
+                        };
+
+                        Console.Write(user);
+                    }
+                    //readers MUST close
+                    reader.Close();
+
+                    //Select multiple Users
+                    SqlCommand cmdUsers = new SqlCommand("SELECT ID, Username, Password FROM UsersDetails", sqlConnection);
+                    SqlDataReader readerUsers = cmdUsers.ExecuteReader();
+
+                    List<User> users = new List<User>();
+
+                    Console.WriteLine("Users: ");
+                    while (readerUsers.Read())
+                    {
+                        User user = new User
+                        {
+                            ID = readerUsers.GetInt32(0),
+                            Username = readerUsers.GetString(1),
+                            Password = readerUsers.GetString(2)
+                        };
+                        users.Add(user);
+                    }
+
+                    //READER MUST CLOSE
+                    readerUsers.Close();
+
+                    foreach (User user in users)
+                    {
+                        Console.WriteLine(user);
+                    }
+
+                    //Update
+                    Console.WriteLine("Enter a Username for Update: ");
+                    string usernameForUpdate = Console.ReadLine();
+
+                    Console.WriteLine("Enter a New password for User: ");
+                    string newPassword = Console.ReadLine();
+
+                    SqlCommand cmdUpdate = new SqlCommand($"UPDATE FROM UsersDetails SET Password = '{newPassword}' " +
+                        $"WHERE Username = '{usernameForUpdate}'", sqlConnection);
+
+                    int rowsUpdated = cmdUpdate.ExecuteNonQuery();
+                    if (rowsUpdated > 0)
+                    {
+                        Console.WriteLine("Updated Successfully.");
+                        Console.WriteLine($"{rowsUpdated} rows updated succefully.");
+                    }
+
+                    //Delete
+                    Console.WriteLine("Enter Username to Delete");
+                    string userToDelete = Console.ReadLine();
+
+                    SqlCommand cmdDelete = new SqlCommand($"DELETE FROM UsersDetails WHERE Username = '{userToDelete}'", sqlConnection);
+
+                    int rowsDeleted = cmdDelete.ExecuteNonQuery();
+                    if (rowsDeleted > 0)
+                    {
+                        Console.WriteLine("Deleted Succefully");
+                        Console.WriteLine($"{rowsDeleted} rows deleted successfully.");
+                    }
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                    Console.WriteLine("SqlConnection closed");
+                }
+            }
+        }
+    }
+}
---
+++ SQLConsoleApp/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLConsoleApp
+{
+    class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+              sb.Append($"User ID is: {ID}")
+                .Append($"Name is: {Username}")
+                .Append($"Password id: {Password}");
+
+            return sb.ToString();
+        }
+    }
+}

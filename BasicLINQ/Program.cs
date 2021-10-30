f98aa040e0d14600663a6841f2359c760b1b932e
nick stavrou <nickstavrou@gmail.com>
2021-10-30 09:51:56 +0300

    new branches

---
+++ BasicLINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = new int[] { 8, 10, 12, 16, 18, 25, 30, 42, 59, 70 };
+            int countAdults = (from age in ages where age >= 18 select age).Count();
+
+
+            int maxAdultAge = (from age in ages where age >= 18 select age).Max();
+            int minAdultAge = (from age in ages where age >= 18 select age).Min();
+            double avgAge = (from age in ages select age).Average();
+
+        }
+    }
+}

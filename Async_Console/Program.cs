23a04ce369aaff5903f1f73e76b41640caef69f0
nick stavrou <nickstavrou@gmail.com>
2021-12-05 16:43:02 +0200

    add simple Async example

---
+++ Async_Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Async_Console
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: Start");
+
+            foreach (var item in await FetchItems())
+            {
+                Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {item}");
+            }
+            #region Async
+            //await foreach (var item in FetchItems())
+            //{
+            //    Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: {item}");
+            //}
+            #endregion
+
+
+            Console.WriteLine($"{DateTime.Now.ToLongTimeString()}: End");
+            Console.ReadKey();
+        }
+
+        static async Task<IEnumerable<int>> FetchItems()
+        {
+            List<int> Items = new();
+            for (int i = 1; i <= 10; i++)
+            {
+                await Task.Delay(1000);
+                Items.Add(i);
+            }
+            return Items;
+        }
+        #region Async
+        //static async IAsyncEnumerable<int> FetchItems()
+        //{
+        //    for (int i = 1; i <= 10; i++)
+        //    {
+        //        await Task.Delay(1000);
+        //        yield return i;
+        //    }
+        //}
+        #endregion
+
+    }
+}
+
--- EntityFrameworkIntro/Program.cs
+++ EntityFrameworkIntro/Program.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
-using static System.Console;
+
 
 namespace EntityFrameworkIntro
 {
@@ -17,59 +17,63 @@ namespace EntityFrameworkIntro
                 var porridgeOats = await context.Dishes.FirstOrDefaultAsync(d => d.Title == "Porridge");
                 if(porridgeOats is null)
                 {
-                    WriteLine("No porridge in the DB");
+                    Console.WriteLine("No porridge in the DB");
+
+                    //insert
+                    //porridgeOats = new Dish { Title = "Porridge", Notes = "Breakfast", RatingStar = 4 };
+                    //context.Dishes.Add(porridgeOats);
+                    //await context.SaveChangesAsync();
+                    
                 }
-                porridgeOats = new Dish { Title = "Porridge", Notes = "Breakfast", RatingStar = 4 };
-                context.Dishes.Add(porridgeOats);
-                await context.SaveChangesAsync();
-                WriteLine($"\tAdded ({JsonSerializer.Serialize(porridgeOats)})");
+                Console.WriteLine($"\tAdded Dish with (id = {porridgeOats.Id})");
 
-                if(porridgeOats.RatingStar == 4)
-                {
-                    //Update rating 
-                    porridgeOats.RatingStar = 5;
-                    //porridgeOats is in memory we do not want to call it from the DB again
-                    await context.SaveChangesAsync();
-                }
+                //if(porridgeOats.RatingStar == 4)
+                //{
+                //    //Update rating 
+                //    porridgeOats.RatingStar = 5;
+                //    //porridgeOats is in memory we do not want to call it from the DB again
+                //    await context.SaveChangesAsync();
+                //}
 
-                //check if we have ingredients in the DB for porridgeOats
-                if (!await context.DishIngredients.AnyAsync(i => i.Dish == porridgeOats))
-                {
-                    WriteLine("Adding Ingerdients");
+                ////check if we have ingredients in the DB for porridgeOats
+                //if (!await context.DishIngredients.AnyAsync(i => i.Dish == porridgeOats))
+                //{
+                //    Console.WriteLine("Adding Ingerdients");
 
-                    //add a single dishingredient
-                    await context.AddAsync(new DishIngredient 
-                    { 
-                        Dish = porridgeOats,
-                        Description = "Oat",
-                        Amount = 50m,
-                        UnitOfMeasure = "gr"                    
-                    });
-                    //add multiple dishingredients
-                    var ingredients = new DishIngredient[]
-                    {
-                            new() { Dish = porridgeOats, Description = "Oat Milk", Amount = 250m, UnitOfMeasure = "gr" },
-                            new() { Dish = porridgeOats, Description = "Cashew Nuts", Amount = 1m, UnitOfMeasure = "hand" },
-                            new() { Dish = porridgeOats, Description = "Poppyseed", Amount = 1m, UnitOfMeasure = "spoon" },
-                            new() { Dish = porridgeOats, Description = "Pumpkin Seed Protein Powder", Amount = 1m, UnitOfMeasure = "spoon" },
-                            new() { Dish = porridgeOats, Description = "Seasonal Fruits", Amount = 2m, UnitOfMeasure = "small apples" }
-                    };
+                //    //add a single dishingredient
+                //    await context.AddAsync(new DishIngredient 
+                //    { 
+                //        Dish = porridgeOats,
+                //        Description = "Oat",
+                //        Amount = 50m,
+                //        UnitOfMeasure = "gr"                    
+                //    });
+                //    //add multiple dishingredients
+                //    var ingredients = new DishIngredient[]
+                //    {
+                //            new() { Dish = porridgeOats, Description = "Oat Milk", Amount = 250m, UnitOfMeasure = "gr" },
+                //            new() { Dish = porridgeOats, Description = "Cashew Nuts", Amount = 1m, UnitOfMeasure = "hand" },
+                //            new() { Dish = porridgeOats, Description = "Poppyseed", Amount = 1m, UnitOfMeasure = "spoon" },
+                //            new() { Dish = porridgeOats, Description = "Pumpkin Seed Protein Powder", Amount = 1m, UnitOfMeasure = "spoon" },
+                //            new() { Dish = porridgeOats, Description = "Seasonal Fruits", Amount = 2m, UnitOfMeasure = "small apples" }
+                //    };
 
-                    await context.AddRangeAsync(ingredients);
-                    await context.SaveChangesAsync();
+                //    await context.AddRangeAsync(ingredients);
+                //    await context.SaveChangesAsync();
 
-                }
+                //}
 
-                //delete dish with its ingredients
-                foreach (var ingredient in await context.DishIngredients
-                    .Where(x => x.DishId == porridgeOats.Id)
-                    .ToArrayAsync())
-                {
-                    context.Remove(ingredient);
-                }
-                context.Remove(porridgeOats);
-                await context.SaveChangesAsync();
+                ////delete dish with its ingredients
+                //foreach (var ingredient in await context.DishIngredients
+                //    .Where(x => x.DishId == porridgeOats.Id)
+                //    .ToArrayAsync())
+                //{
+                //    context.Remove(ingredient);
+                //}
+                //context.Remove(porridgeOats);
+                //await context.SaveChangesAsync();
             }
+            Console.ReadKey();
         }
     }
 
